051f47c01a8f27d733c95c3f06a26673380689b4
Tanmay Jain <71631895+TanmayJain97@users.noreply.github.com>
2020-09-28 23:41:31 +0530

    Updated file UC 1

---
+++ RegexPinCode.java
@@ -0,0 +1,32 @@
+package com.regex_pin_code;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegexPinCode {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		
+		String match = "[1-9]{1}[0-9]{5}";			//String pattern to be detected
+
+		//Checking for matches
+		while(true) {
+			
+			// String to be scanned to find the pattern.
+			System.out.print("Please enter PIN Code: ");
+			String line = sc.next();
+			
+			boolean check=line.matches(match);							//Condition check
+			if(check) {
+				break;
+			}else{
+				System.out.println("Incorrect format. Please try again.");
+				System.out.println();
+			}
+		}
+		System.out.println("Program End.");
+	}
+}
